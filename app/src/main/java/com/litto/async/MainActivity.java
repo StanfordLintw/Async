f8f3f1bd2f7e31022f76b1bb5a0e32c92b9e53c0
Hank Tom <hanktom@gmail.com>
2017-11-16 20:36:14 +0800

    Job2, Job3

--- app/src/main/java/com/litto/async/MainActivity.java
+++ app/src/main/java/com/litto/async/MainActivity.java
@@ -21,10 +21,54 @@ public class MainActivity extends AppCompatActivity {
         new Job1Task().execute();
     }
     public void go2(View view){
-
+        new Job2Task().execute(3);
     }
     public void go3(View view){
+        new Job3Task().execute(6);
+    }
 
+    class Job3Task extends AsyncTask<Integer, Integer, Void>{
+
+        @Override
+        protected Void doInBackground(Integer... integers) {
+            for (int i=0; i<integers[0]; i++){
+                try {
+                    publishProgress(i);
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            info.setText(values[0]+"");
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            info.setText("DONE");
+        }
+    }
+
+    class Job2Task extends AsyncTask<Integer, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Integer... integers) {
+            try {
+                Thread.sleep(integers[0]*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            info.setText("DONE");
+        }
     }
 
     class Job1Task extends AsyncTask<Void, Void, Void>{
@@ -36,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
             return null;
         }
 
