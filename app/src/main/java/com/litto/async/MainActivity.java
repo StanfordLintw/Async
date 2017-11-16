111fa26e6b3046eda38f5b43742fc061224cd67e
Hank Tom <hanktom@gmail.com>
2017-11-16 20:21:20 +0800

    Job1Task

--- app/src/main/java/com/litto/async/MainActivity.java
+++ app/src/main/java/com/litto/async/MainActivity.java
@@ -1,13 +1,49 @@
 package com.litto.async;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        info = findViewById(R.id.info);
     }
+
+    public void go1(View view){
+        new Job1Task().execute();
+    }
+    public void go2(View view){
+
+    }
+    public void go3(View view){
+
+    }
+
+    class Job1Task extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            info.setText("DONE");
+        }
+    }
+
 }
